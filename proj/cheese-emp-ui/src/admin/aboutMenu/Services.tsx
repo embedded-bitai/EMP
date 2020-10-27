ded7b9cf5e249c5bf6bbe9f0e0f7a0362a8f111f
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-10-27 17:52:30 +0900

    17:52, proj 폴더 재업로드

---
+++ proj/cheese-emp-ui/src/admin/aboutMenu/Services.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Service = () => <div>
+    <h1>서비스 내용</h1>
+</div>
+
+export default Service
---
+++ proj/cheese-emp-ui/src/survey/useAnimatedNavToggler.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useAnimation, useCycle } from "framer-motion";
+
+export default function useAnimatedNavToggler() {
+  const [showNavLinks, setShowNavLinks] = useState(false);
+  const [x, cycleX] = useCycle("0%", "150%");
+  const animation = useAnimation();
+
+  const toggleNavbar = () => {
+    setShowNavLinks(!showNavLinks);
+    animation.start({ x: x, display: "block" });
+    cycleX();
+  };
+
+  return {showNavLinks,animation, toggleNavbar }
+}
